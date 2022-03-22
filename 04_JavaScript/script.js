c430497cb1b66dfadc8a155ca6ee5b42941e709d
Fatih Deniz <deniz.fatih@hotmail.de>
2022-03-22 22:19:46 +0100

    Javascript

---
+++ 04_JavaScript/script.js
@@ -0,0 +1,17 @@
+console.log("Merhaba");
+console.log(1234);
+console.log(true);
+console.log([1, 2, 3]);
+console.error("Hata olustu");
+console.warn("bir uyari olustu");
+
+//tek satirlik yorum satiri
+
+/*
+yorum satirlari
+yorum satirlari
+yorum satirlari
+
+*/
+
+console.clear();
