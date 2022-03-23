17cd401642d0b00df6ccdf9408841257b1810306
Fatih Deniz <deniz.fatih@hotmail.de>
2022-03-23 18:23:31 +0100

    Javascript

--- 04_JavaScript/7-Uygulama Operatörler/script.js
+++ 04_JavaScript/7-Uygulama Operatörler/script.js
@@ -1,74 +1,3 @@
-// // Operatörler
-
-// let val;
-// const a=10;
-// const b=5;
-// const c=5;
-// let   d=3;
-
-// // 1- Aritmetik Operatörler
-
-// val = a+b;
-// val = a-b;
-// val = a*b;
-// val = a/b;
-// val = a%b;
-// val = d++;
-// val = ++d;
-// val = --d;
-// val = d--;
-
-// // 2- Atama Operatörleri
-
-// val = a;
-// val +=a; // val = val + a;
-// val -=a; // val = val - a;
-// val *=a; // val = val * a;
-// val /=a; // val = val / a;
-// val %=a; // val = val % a;
-
-
-// // 3- Karşılaştırma Operatörleri
-
-// val = a==b;
-// val = b==c;
-// val = b===c; // değer kontrolü & type
-// val = 5 === '5';
-// val = a!=b;
-// val = a!==b;
-// val = a > b;
-// val = b < a;
-// val = a >= b;
-// val = 5 >= 5;
-// val = a <= b;
-
-// // 4- Mantıksal Operatörler
-
-//    // && (And)
-//    // true  && true  => true
-//    // true  && false => false
-//    // false && false => false
-
-//    val=  (a>b) && (a>c) 
-//    val = (a>b) || (a<c)
-//    val = !(a>b)
-
-
-//    // || (Or)
- 
-//    // true  && true  => true
-//    // true  && false => true
-//    // false && false => false
-
-//    // ! (Not)
-
-//    // !true  => false
-//    // !false => true
-
-// console.log(val);
-// console.log(typeof val);
-
-
 // Demo : Operators
 
 // 1 - Can ve Ada 'nın boy ve kg bilgilerini alın.
@@ -88,32 +17,31 @@ let IndexAda;
 const kgCan = 60;
 const kgAda = 40;
 
-const heigtCan = 1.70;
-const heigtAda = 1.50;
+const heigtCan = 1.7;
+const heigtAda = 1.5;
 
-IndexAda = (kgAda) / (heigtAda*heigtAda);
-IndexCan = (kgCan) / (heigtCan*heigtCan);
+IndexAda = kgAda / (heigtAda * heigtAda);
+IndexCan = kgCan / (heigtCan * heigtCan);
 
-console.log(IndexAda.toFixed(2),IndexCan.toFixed(3));
+console.log(IndexAda.toFixed(2), IndexCan.toFixed(3));
 
-let adaHigherIndex = IndexAda>IndexCan;
-let canHigherIndex = IndexCan>IndexAda;
+let adaHigherIndex = IndexAda > IndexCan;
+let canHigherIndex = IndexCan > IndexAda;
 
-console.log("Ada'nın kilo indeksi can'ın kilo indeksinden daha büyük : "+adaHigherIndex);
+console.log(
+  "Ada'nın kilo indeksi can'ın kilo indeksinden daha büyük : " + adaHigherIndex
+);
 
-console.log("Can'ın kilo indeksi ada'nın kilo indeksinden daha büyük : "+canHigherIndex);
+console.log(
+  "Can'ın kilo indeksi ada'nın kilo indeksinden daha büyük : " + canHigherIndex
+);
 
-let AdaZayif = (IndexAda>=0) && (IndexAda<=18.4);
-let AdaNormal = (IndexAda>=18.5) && (IndexAda<=24.9);
-let AdaKilolu = (IndexAda>=25) && (IndexAda<=29.9);
-let AdaSisman = (IndexAda>=30) && (IndexAda<=34.9);
+let AdaZayif = IndexAda >= 0 && IndexAda <= 18.4;
+let AdaNormal = IndexAda >= 18.5 && IndexAda <= 24.9;
+let AdaKilolu = IndexAda >= 25 && IndexAda <= 29.9;
+let AdaSisman = IndexAda >= 30 && IndexAda <= 34.9;
 
-console.log("Ada zayıf :"+AdaZayif);
-console.log("Ada'nın kilosu normal : "+AdaNormal);
-console.log("Ada kilolu mu : "+AdaKilolu);
-console.log("Ada şişman mı : "+AdaSisman);
-
-
-
-
-
+console.log("Ada zayıf :" + AdaZayif);
+console.log("Ada'nın kilosu normal : " + AdaNormal);
+console.log("Ada kilolu mu : " + AdaKilolu);
+console.log("Ada şişman mı : " + AdaSisman);
