51dd624e689d3647e449f6763bd545748d9fb7bb
Fatih Deniz <deniz.fatih@hotmail.de>
2022-05-15 22:52:30 +0200

    Jscript

---
+++ 04_JavaScript/Bölüm -3 - OOP/6-Sorularin Gösterilmesi - Prototype Tabanlı Kalıtım/script.js
@@ -0,0 +1,60 @@
+// OOP: Nesne Tabanlı Programlama
+
+function Soru(soruMetni, cevapSecenekleri, dogruCevap) {
+    this.soruMetni = soruMetni;
+    this.cevapSecenekleri = cevapSecenekleri;
+    this.dogruCevap = dogruCevap;
+}
+
+Soru.prototype.cevabiKontrolEt = function(cevap) {
+    return cevap === this.dogruCevap
+}
+
+let sorular = [
+    new Soru("1-Hangisi javascript paket yönetim uygulasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" , d: "Nuget" }, "c"),
+    new Soru("2-Hangisi javascript paket yönetim uygulasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" }, "c"),
+    new Soru("3-Hangisi javascript paket yönetim uygulasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" }, "c"),
+    new Soru("4-Hangisi javascript paket yönetim uygulasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" }, "c")
+];
+
+function Quiz(sorular) {
+    this.sorular = sorular;
+    this.soruIndex = 0;
+}
+
+Quiz.prototype.soruGetir = function() {
+    return this.sorular[this.soruIndex];
+}
+
+const quiz = new Quiz(sorular);
+
+document.querySelector(".btn_start").addEventListener("click", function() {
+    document.querySelector(".quiz_box").classList.add("active");
+    soruGoster(quiz.soruGetir());
+})
+
+document.querySelector(".next_btn").addEventListener("click", function() {
+    if (quiz.sorular.length != quiz.soruIndex + 1) {
+        quiz.soruIndex += 1;
+        soruGoster(quiz.soruGetir());
+    } else {
+        console.log("quiz bitti");
+    }
+});
+
+function soruGoster(soru) {
+    let question = `<span>${soru.soruMetni}</span>`;
+    let options = '';
+
+    for(let cevap in soru.cevapSecenekleri) {
+        options += 
+            `
+                <div class="option"> 
+                    <span><b>${cevap}</b>: ${soru.cevapSecenekleri[cevap]}</span>
+                </div>
+            `;
+    }
+
+    document.querySelector(".question_text").innerHTML = question;
+    document.querySelector(".option_list").innerHTML = options;
+}
