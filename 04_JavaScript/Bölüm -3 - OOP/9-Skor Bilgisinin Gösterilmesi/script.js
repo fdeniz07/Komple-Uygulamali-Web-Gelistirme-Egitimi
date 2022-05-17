00663767a4afa32c6122db934920d9f6d57ca67d
Fatih Deniz <deniz.fatih@hotmail.de>
2022-05-18 00:59:08 +0200

    Javascript

---
+++ 04_JavaScript/Bölüm -3 - OOP/9-Skor Bilgisinin Gösterilmesi/script.js
@@ -0,0 +1,57 @@
+const quiz = new Quiz(sorular);
+const ui = new UI();
+
+ui.btn_start.addEventListener("click", function() {
+    ui.quiz_box.classList.add("active");
+    ui.soruGoster(quiz.soruGetir());
+    ui.soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.length);
+    ui.btn_next.classList.remove("show");
+});
+
+ui.btn_next.addEventListener("click", function() {
+    if (quiz.sorular.length != quiz.soruIndex + 1) {
+        quiz.soruIndex += 1;
+        ui.soruGoster(quiz.soruGetir());
+        ui.soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.length);
+        ui.btn_next.classList.remove("show");
+    } else {
+        console.log("quiz bitti");
+        ui.quiz_box.classList.remove("active");
+        ui.score_box.classList.add("active");
+        ui.skoruGoster(quiz.sorular.length, quiz.dogruCevapSayisi);
+    }
+});
+
+ui.btn_quit.addEventListener("click", function() {
+    window.location.reload();
+});
+
+ui.btn_replay.addEventListener("click", function() {
+    quiz.soruIndex = 0;
+    quiz.dogruCevapSayisi = 0;
+    ui.btn_start.click();
+    ui.score_box.classList.remove("active");
+});
+
+
+function optionSelected(option) {
+    let cevap = option.querySelector("span b").textContent;
+    let soru = quiz.soruGetir();
+
+    if(soru.cevabiKontrolEt(cevap)) {
+        quiz.dogruCevapSayisi += 1;
+        option.classList.add("correct");
+        option.insertAdjacentHTML("beforeend", ui.correctIcon);
+    } else {
+        option.classList.add("incorrect");
+        option.insertAdjacentHTML("beforeend", ui.incorrectIcon);
+    }
+
+    for(let i=0; i < ui.option_list.children.length; i++) {
+        ui.option_list.children[i].classList.add("disabled");
+    }
+
+    ui.btn_next.classList.add("show");
+}
+
+
